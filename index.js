89ca07ead4bd02e614feb22bc0089d41f08ccf0b
JP <jpzarza@jpzarza.local>
2023-01-26 15:30:18 -0600

    Fixed starting issues

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3030;
 
 app.use(Gun.serve);
-const server = app.listen(port, () => {
+const server = app.listen( process.env.PORT || port, () => {
     console.log(`App live at ${port}`);
 })
 Gun({ web: server })
