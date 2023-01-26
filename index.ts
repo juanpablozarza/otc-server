ecbf4112e1058a2d465e845cfd1ead24f3bf8836
JP <jpzarza@jpzarza.local>
2023-01-26 14:53:01 -0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+const Gun = require('gun');
+const express = require('express');
+
+const app = express();
+const port = 3030;
+
+app.use(Gun.serve);
+const server = app.listen(port, () => {
+    console.log(`App live at ${port}`);
+})
+Gun({ web: server })
