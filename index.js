bd4b603d6bad87626f5c7ce3c5fe2ad6adc1ec8f
JP <jpzarza@jpzarza.local>
2023-02-28 18:52:35 -0600

    Added google sheets api connection

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const Gun = require('gun');
 const express = require('express');
+const cors = require('cors');
+const { google } = require('googleapis');
 
 const app = express();
 const port = 3030;
+app.get('/:state/:method', async (req, res) => {
+    try {
+        const { state, method } = req.params;
+        const auth = await google.auth.getClient({
+            keyFile: './credentials-otc.json',
+            scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        });
+        const sheets = google.sheets({
+            version: "v4",
+            auth: auth
+        });
+        const range = `Mexico!A1:F30`;
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId: '1JNw1fLcav0HUD_802gwsN5EsfQK_P5Ti1RAfnqheIiw',
+            range: range
+        });
+        res.status(200);
+        res.send({
+            data: response.data.values,
+            ok: true
+        });
 
-app.use(Gun.serve);
-const server = app.listen( process.env.PORT || port, () => {
+    } catch (error) {
+        res.status(305);
+        res.send({
+            error: error.toString()
+        })
+
+    }
+
+
+});
+app.use(Gun.serve, [express.json({ limit: '50mb' }), cors()]);
+const server = app.listen(process.env.PORT || port, () => {
     console.log(`App live at ${port}`);
 })
-Gun({ web: server })
+Gun({ web: server });
+
